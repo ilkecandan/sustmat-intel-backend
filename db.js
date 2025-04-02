62ff67f7b58a9a30d97c7696c509af3ee1c86bdc
İlke Candan Bengi <64421962+ilkecandan@users.noreply.github.com>
2025-04-02 20:40:01 +0300

    Update db.js

--- db.js
+++ db.js
@@ -1,1 +1,24 @@
+// db.js
+import pg from "pg";
+import dotenv from "dotenv";
+dotenv.config();
+
+const { Pool } = pg;
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: { rejectUnauthorized: false },
+});
+
+export const testDB = async () => {
+  try {
+    const res = await pool.query("SELECT NOW()");
+    return res.rows[0];
+  } catch (err) {
+    console.error("DB error:", err);
+    return null;
+  }
+};
+
+export default pool;
 
