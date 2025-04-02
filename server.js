e03305d5d979df7844ab1d15aee27086b339ef1c
İlke Candan Bengi <64421962+ilkecandan@users.noreply.github.com>
2025-04-02 20:39:43 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,1 +1,19 @@
+// server.js
+import express from "express";
+import dotenv from "dotenv";
+import { testDB } from "./db.js";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/", async (req, res) => {
+  const result = await testDB();
+  res.json({ status: "Online", db: result });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
